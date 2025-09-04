b8dd93c349371985ca0ecc12e5e31a36fc5081a8
arielfainshtein <arielfainshtein@icloud.com>
2025-09-04 10:00:14 +0300

    implemented basic canPlaceEntity method.

--- src/game/scenes/Editor/Editor.ts
+++ src/game/scenes/Editor/Editor.ts
@@ -2,9 +2,16 @@ import { Scene } from 'phaser';
 import { EventBus } from '../../EventBus';
 import { EntityType } from './EditorUI';
 
+/* TODO: Implement entity placement logic
+    * - Listen for 'editor-place-entity' events from EditorUI ✓
+    * - build function to check if new entity can be placed at the given coordinates (no overlap, within bounds, etc.)
+*/
+
+
 export class Editor extends Scene {
 
     private platforms: Phaser.Physics.Arcade.StaticGroup;
+    private allObjects: Phaser.GameObjects.Group;
 
     constructor() {
         super('Editor');
@@ -14,8 +21,9 @@ export class Editor extends Scene {
         this.add.image(400, 600, 'background');
 
         EventBus.on('editor-place-entity', this.addEntity, this);
-        
+
         this.platforms = this.physics.add.staticGroup();
+        this.allObjects = this.add.group();
 
         // Close EventBus listener
         this.events.once(Phaser.Scenes.Events.SHUTDOWN, () => {
@@ -25,10 +33,10 @@ export class Editor extends Scene {
         EventBus.emit('current-scene-ready', this);
     }
 
-    private addEntity({entityType, x, y}: {entityType: EntityType, x: number, y: number}) {
+    private addEntity({ entityType, x, y }: { entityType: EntityType, x: number, y: number }) {
         // Logic to add the entity to the scene
         console.log('Adding entity:', entityType, 'at', x, y);
-        switch(entityType) {
+        switch (entityType) {
             case 'platform':
                 console.log('Adding platform at', x, y);
                 this.addPlatform(x, y);
@@ -43,7 +51,35 @@ export class Editor extends Scene {
     }
 
     private addPlatform(x: number, y: number) {
-        this.platforms.create(x, y, 'platform');
+        // this.platforms.create(x, y, 'platform');
+
+        if (this.canPlaceEntity(x, y, 100, 100)) {
+            const rect = this.add.rectangle(x, y, 100, 100, 0x000000);
+            this.platforms.add(rect);
+            this.allObjects.add(rect);
+        }
+
+        
+    }
+
+    // todo: also account for scene bounds when camera is set up
+    private canPlaceEntity(x: number, y: number, objectWidth: number, objectHeight: number): boolean {
+        const potentialBounds = new Phaser.Geom.Rectangle(
+            x - objectWidth / 2,
+            y - objectHeight / 2,
+            objectWidth,
+            objectHeight
+        );
+
+        const objects = this.allObjects.getChildren() as (Phaser.GameObjects.Sprite | Phaser.GameObjects.Image)[];
+        for (const obj of objects) {
+            const objBounds = obj.getBounds();
+            if (Phaser.Geom.Intersects.RectangleToRectangle(potentialBounds, objBounds)) {
+                return false;
+            }
+        }
+
+        return true;
     }
 
 }
