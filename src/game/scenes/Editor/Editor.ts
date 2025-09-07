41407bb3b52d73b0d26356aae28348fcbcc5c711
arielfainshtein <arielfainshtein@icloud.com>
2025-09-07 11:01:16 +0300

    implemented basic grid

--- src/game/scenes/Editor/Editor.ts
+++ src/game/scenes/Editor/Editor.ts
@@ -2,32 +2,57 @@ import { Scene } from 'phaser';
 import { EventBus } from '../../EventBus';
 import { EntityType } from './EditorUI';
 
-/* TODO: Implement entity placement logic
-    * - Listen for 'editor-place-entity' events from EditorUI ✓
-    * - build function to check if new entity can be placed at the given coordinates (no overlap, within bounds, etc.)
-    * - when an entity is in close proximity to a platform make the entity stick to the platform.
-*/
+
 
 
 export class Editor extends Scene {
+
+    cursors: Phaser.Types.Input.Keyboard.CursorKeys;
 
     constructor() {
         super('Editor');
     }
 
     create() {
-        this.add.image(400, 600, 'background');
+        this.add.image(400, 300, 'background').setScrollFactor(0).setDepth(-10);
 
-        
+        const g1 = this.add.grid(0, 0, 800, 600, 40, 40).setOrigin(0,0)
+        g1.setOutlineStyle(0x000000,1);
+
+         this.cameras.main.setBounds(0, 0, 800, 600);
+         this.physics.world.setBounds(0, 0, 800, 600);
+
+        this.cursors = this.input.keyboard!.createCursorKeys();
+
+
         EventBus.emit('current-scene-ready', this);
     }
 
-    
+    update() {
 
-    
 
-    
+        // handle camera scroll
+        if (this.cursors.left.isDown) {
+            this.cameras.main.scrollX -= 40;
+        }
+        else if (this.cursors.right.isDown) {
+            this.cameras.main.scrollX += 40;
+        }
 
-    
+        if (this.cursors.up.isDown) {
+            this.cameras.main.scrollY -= 40;
+        }
+
+        if (this.cursors.down.isDown) {
+            this.cameras.main.scrollY += 40;
+        }
+    }
+
+
+
+
+
+
+
 
 }
