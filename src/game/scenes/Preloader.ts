48a5105afd9c45ec6ae05cfcfaeef6e0c5512a12
arielfainshtein <arielfainshtein@icloud.com>
2025-09-07 09:29:20 +0300

    Changed temporarily to start scene at Editor right away for easier testing.

--- src/game/scenes/Preloader.ts
+++ src/game/scenes/Preloader.ts
@@ -26,6 +26,9 @@ export class Preloader extends Scene
         //  For example, you can define global animations here, so we can use them in other scenes.
 
         //  Move to the MainMenu. You could also swap this for a Scene Transition, such as a camera fade.
-        this.scene.start('MainMenu');
+        //this.scene.start('MainMenu');
+
+        //for easier work. change the mainmenu later.
+        this.scene.start('Editor');
     }
 }
